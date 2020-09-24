11e8cdf22603c0547690c3bf2fcf0ed4064c21a3
hojin Mun <jin1281@gmail.com>
2020-09-23 22:56:07 -0400

    Updates

---
+++ precache-manifest.2df69e52d4f62760b1499c34ae1896cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ecbfd1bd1f6f7c0c4727a3b5734e9e",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "594dff79c58b5ee95742",
+    "url": "/myportfolio/static/css/main.8b5d00fa.chunk.css"
+  },
+  {
+    "revision": "7d22f5384f2954b67e9b",
+    "url": "/myportfolio/static/js/2.452781c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.452781c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594dff79c58b5ee95742",
+    "url": "/myportfolio/static/js/main.9de847d0.chunk.js"
+  },
+  {
+    "revision": "4d34866d393e0cb58c69",
+    "url": "/myportfolio/static/js/runtime-main.79215d36.js"
+  }
+]);
